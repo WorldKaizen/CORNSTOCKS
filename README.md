d40a70236744e95751a06b76b2ce648d2a4dd9cd
WorldKaizen <73084934+WorldKaizen@users.noreply.github.com>
2021-05-22 22:00:55 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# CORNSTOCKS
- 
+# CornStocks
+
+## Description
+
+CornStocks helps investors track their stocks by providing live market data. The convenient notes feature is displayed along with the stock tracker and allows users to document their thoughts, ideas, research and important dates all on one page. 
+  
+## Built With 
+
+  * HTML
+  * JavaScript
+  * CSS
+  * BootStrap
+  * Axios
+  * Express
+  * Bcrypt
+  * MySql2
+  * Dotenv
+  * Handlebars
+  * Sequelize
+
+## Links
+
+Live Page: https://cornstocks-10-team.herokuapp.com/
+
+GitHub Repository: https://azun510.github.io/Cornstocks/
+
+
+## Images
+
+![alt text](public/stylesheets/stock-readme.png)
+
+![alt text](public/stylesheets/login-readme.png)
+
+![alt text](public/stylesheets/homepage-readme.png)
---
+++ server.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const apiroutes=require("./apiroutes/apiroute");
+// const htmlRoutes=require("./apiroutes/htmlRoutes")
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+apiroutes(app)
+// htmlRoutes(app)
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: { maxAge: 600000 },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening at http://localhost:${PORT}` ));
+});
